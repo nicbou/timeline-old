c7fbd792f29e2979f3272da9f8a0ea85d7a17365
Nicolas Bouliane <contact@nicolasbouliane.com>
2020-10-21 10:12:07 +0200

    Made backup key unique

--- backend/source/backup/models.py
+++ backend/source/backup/models.py
@@ -6,7 +6,7 @@ class BackupSource(models.Model):
     host = models.CharField(max_length=255, blank=False)
     port = models.PositiveSmallIntegerField(default=22)
     path = models.TextField(blank=False)
-    key = models.CharField(max_length=80, blank=False)
+    key = models.CharField(max_length=80, blank=False, unique=True)
 
     def __str__(self):
         return f"{self.key} ({self.user}@{self.host}:{self.port}, {self.path})"
