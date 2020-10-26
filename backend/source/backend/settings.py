83084433edb2f940021c81ec819dbfd544976462
Nicolas Bouliane <contact@nicolasbouliane.com>
2020-10-26 10:53:45 +0100

    Allow filtering of entries

--- backend/source/backend/settings.py
+++ backend/source/backend/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'timeline.apps.TimelineConfig',
     'backup.apps.BackupConfig',
--- backend/source/timeline/views.py
+++ backend/source/timeline/views.py
@@ -2,9 +2,15 @@ from .models import Entry
 from rest_framework import permissions
 from rest_framework import viewsets
 from .serializers import EntrySerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
 
 class EntryViewSet(viewsets.ModelViewSet):
     queryset = Entry.objects.all().order_by('-date_on_timeline')
     serializer_class = EntrySerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
     permission_classes = [permissions.AllowAny]
+    filterset_fields = {
+        'date_on_timeline':['gte', 'lte', 'exact', 'gt', 'lt'],
+    }
