477cc604b4f00ba4a55a3c17ebfbcdf8e4a94dd2
Nicolas Bouliane <contact@nicolasbouliane.com>
2020-10-28 16:00:22 +0100

    Improved date nav

--- frontend/source/js/components/timeline-nav.js
+++ frontend/source/js/components/timeline-nav.js
@@ -2,22 +2,35 @@ export default Vue.component('timeline-nav', {
   computed: {
     timelineDate: {
       get() {
-        return this.$store.state.timeline.timelineDate.format('YYYY-MM-DD');
+        return this.$store.state.timeline.timelineDate;
       },
       set(newDate) {
+        newDate = moment.min(newDate, this.today);
         return this.$store.dispatch('timeline/setTimelineDate', moment(newDate));
       }
     },
     timelineDateIso: {
       get() {
-        return this.$store.state.timeline.timelineDate;
+        return this.timelineDate.format('YYYY-MM-DD');
       },
       set(newDate) {
-        return this.$store.dispatch('timeline/setTimelineDate', newDate);
+        return this.timelineDate = moment(newDate);
       }
     },
     today: function(){
       return moment().startOf('day');
+    },
+    showTomorrow: function() {
+      return moment(this.timelineDate).add('days', 1).diff(this.today) <= 0
+    },
+    showNextWeek: function() {
+      return moment(this.timelineDate).add('weeks', 1).diff(this.today) <= 0
+    },
+    showNextMonth: function() {
+      return moment(this.timelineDate).add('months', 1).diff(this.today) <= 0
+    },
+    showNextYear: function() {
+      return moment(this.timelineDate).add('years', 1).diff(this.today) <= 0
     },
   },
   methods: {
@@ -34,12 +47,12 @@ export default Vue.component('timeline-nav', {
       <button @click="moveTimelineDate('months', -1)">-1M</button>
       <button @click="moveTimelineDate('weeks', -1)">-1W</button>
       <button @click="moveTimelineDate('days', -1)">-1D</button>
-      <input type="date" v-model="timelineDate">
+      <input type="date" v-model="timelineDateIso">
       <button @click="pickTimelineDate(today)">Today</button>
-      <button @click="moveTimelineDate('days', 1)">+1D</button>
-      <button @click="moveTimelineDate('weeks', 1)">+1W</button>
-      <button @click="moveTimelineDate('months', 1)">+1M</button>
-      <button @click="moveTimelineDate('years', 1)">+1Y</button>
+      <button :disabled="!showTomorrow" @click="moveTimelineDate('days', 1)">+1D</button>
+      <button :disabled="!showNextWeek" @click="moveTimelineDate('weeks', 1)">+1W</button>
+      <button :disabled="!showNextMonth" @click="moveTimelineDate('months', 1)">+1M</button>
+      <button :disabled="!showNextYear" @click="moveTimelineDate('years', 1)">+1Y</button>
     </nav>
   `
 });
