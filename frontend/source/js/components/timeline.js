0e71099d07a6fcb8f379e74a29ef99c0e7c5415d
Nicolas Bouliane <contact@nicolasbouliane.com>
2020-10-28 09:08:45 +0100

    Basic timeline navigation, for testing

--- frontend/source/js/components/timeline.js
+++ frontend/source/js/components/timeline.js
@@ -8,6 +8,9 @@ export default Vue.component('timeline', {
     timelineDate: function() {
       return this.$store.state.timeline.timelineDate;
     },
+    relativeTimelineDate: function() {
+      return moment.duration(this.timelineDate.diff(moment().startOf('day'))).humanize(true);
+    },
     entries: function() {
       return this.$store.state.timeline.entries;
     },
@@ -16,20 +19,32 @@ export default Vue.component('timeline', {
     this.$store.dispatch('timeline/getEntries');
   },
   methods: {
-    setTimelineDate: function(quantity, unit) {
+    pickTimelineDate: function(event) {
+      const newDate = moment(event.target.valueAsNumber);
+      return this.$store.dispatch('timeline/setTimelineDate', newDate);
+    },
+    moveTimelineDate: function(quantity, unit) {
       const newDate = moment(this.$store.state.timeline.timelineDate).add(quantity, unit);
       return this.$store.dispatch('timeline/setTimelineDate', newDate);
     },
   },
   template: `
     <div id="timeline" class="container">
-      <button @click="setTimelineDate(-1, 'days')">Prev</button>
-      <button @click="setTimelineDate(1, 'days')">Next</button>
-      <h2>{{ timelineDate }}</h2>
-      {{ entries }}
-      <div class="timeline-items">
-        <div class="timeline-item" v-for="entry in entries" v-if="entry.extra_attributes.preview" :key="entry.id">
-          <span class="title">{{ entry.title }}</span>
+      <button @click="moveTimelineDate(-1, 'years')">-1Y</button>
+      <button @click="moveTimelineDate(-1, 'months')">-1M</button>
+      <button @click="moveTimelineDate(-1, 'weeks')">-1W</button>
+      <button @click="moveTimelineDate(-1, 'days')">-1D</button>
+      <input type="date" :value="timelineDate.format('YYYY-MM-DD')" @change="pickTimelineDate"/>
+      <button @click="moveTimelineDate(1, 'days')">+1D</button>
+      <button @click="moveTimelineDate(1, 'weeks')">+1W</button>
+      <button @click="moveTimelineDate(1, 'months')">+1M</button>
+      <button @click="moveTimelineDate(1, 'years')">+1Y</button>
+      <h2>
+        {{ timelineDate.format('LLLL') }}
+        <small>{{ relativeTimelineDate }}</small>
+      </h2>
+      <div class="tiles">
+        <div class="tile" v-for="entry in entries" v-if="entry.extra_attributes.previews" :key="entry.id">
           <img
             :alt="entry.title"
             :src="entry.extra_attributes.previews.small"/>
