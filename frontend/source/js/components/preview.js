9ddde5f8ccb8c76b0169b34f2f031efdf82a5ab2
Nicolas Bouliane <contact@nicolasbouliane.com>
2020-10-31 17:00:47 +0100

    Video preview on hover

--- frontend/source/js/components/preview.js
+++ frontend/source/js/components/preview.js
@@ -8,7 +8,8 @@ export default Vue.component('preview', {
   template: `
     <div class="preview">
       <button class="close" @click="close">Close</button>
-      <img :alt="entry.title" :src="entry.extra_attributes.previews.large || entry.extra_attributes.previews.small"/>
+      <img v-if="entry.extra_attributes.previews.small.endsWith('.jpg')" :alt="entry.title" :src="entry.extra_attributes.previews.large"/>
+      <video autoplay controls v-if="entry.extra_attributes.previews.small.endsWith('.mp4')" :alt="entry.title" :src="entry.extra_attributes.previews.small"/>
     </div>
   `
 });
--- frontend/source/js/components/timeline-tile.js
+++ frontend/source/js/components/timeline-tile.js
@@ -14,7 +14,21 @@ export default Vue.component('tile', {
     },
     tileClasses: function() {
       return [this.entry.schema.replace('.', '-')]
-    }
+    },
+    isVideo: function() {
+      return (
+        this.entry.extra_attributes.previews
+        && this.entry.extra_attributes.previews.small
+        && this.entry.extra_attributes.previews.small.endsWith('.mp4')
+      );
+    },
+    isImage: function() {
+      return (
+        this.entry.extra_attributes.previews
+        && this.entry.extra_attributes.previews.small
+        && this.entry.extra_attributes.previews.small.endsWith('.jpg')
+      );
+    },
   },
   methods: {
     pickTimelineDate: function(event) {
@@ -25,13 +39,18 @@ export default Vue.component('tile', {
       const newDate = moment(this.$store.state.timeline.timelineDate).add(quantity, unit);
       return this.$store.dispatch('timeline/setTimelineDate', newDate);
     },
+    videoHoverStart: function() {
+      this.$refs.videoElement.play()
+    },
+    videoHoverEnd: function() {
+      this.$refs.videoElement.pause()
+      this.$refs.videoElement.currentTime = 0;
+    },
   },
   template: `
     <div class="tile" v-if="entry.extra_attributes.previews" :style="tileStyle" :class="tileClasses" @click="$emit('click', entry)">
-      <img
-        :alt="entry.title"
-        :src="entry.extra_attributes.previews.small"
-        loading="lazy"/>
+      <img loading="lazy" v-if="isImage" :alt="entry.title" :src="entry.extra_attributes.previews.small"/>
+      <video ref="videoElement" @mouseover="videoHoverStart" @mouseleave="videoHoverEnd" v-if="isVideo" :alt="entry.title" :src="entry.extra_attributes.previews.small" loop/>
     </div>
   `
 });
