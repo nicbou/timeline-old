7deeec126c811d0914bd585ca0b6af99e6ec495f
Nicolas Bouliane <contact@nicolasbouliane.com>
2020-12-22 17:06:43 +0100

    Wrapped tweet in p tag. Fixed #2

--- frontend/source/js/components/tiles/post.js
+++ frontend/source/js/components/tiles/post.js
@@ -45,10 +45,16 @@ export default Vue.component('post-tile', {
       if(this.entry.schema === 'social.reddit.post') {
         return `<h3><a href="${this.entry.extra_attributes.post_url}">${this.entry.title}</a></h3>`;
       }
-      return this.entry.extra_attributes.post_body_html || this.entry.description
-        .replace(/@([\w]{1,50})/ig, '<a target="_blank" href="https://twitter.com/$1">@$1</a>')
-        .replace(/\/r\/([\w]{1,20})/ig, '<a target="_blank" href="https://reddit.com/r/$1">@$1</a>')
-        .replace(/\/u\/([\w]{1,20})/ig, '<a target="_blank" href="https://reddit.com/user/$1">@$1</a>');
+      let output = this.entry.extra_attributes.post_body_html || this.entry.description;
+      if (this.entry.schema === 'social.twitter.tweet') {
+        return `<p>${output}</p>`
+          .replace(/@([\w]{1,50})/ig, '<a target="_blank" href="https://twitter.com/$1">@$1</a>')
+      }
+      else if (this.entry.schema === 'social.reddit.comment') {
+        return output
+          .replace(/\/r\/([\w]{1,20})/ig, '<a target="_blank" href="https://reddit.com/r/$1">@$1</a>')
+          .replace(/\/u\/([\w]{1,20})/ig, '<a target="_blank" href="https://reddit.com/user/$1">@$1</a>');
+      }
     },
   },
   template: `
