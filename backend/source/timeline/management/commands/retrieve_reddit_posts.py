c9cf0c8183194c96269621c62f0d1e314b4375b1
Nicolas Bouliane <contact@nicolasbouliane.com>
2020-12-27 14:07:19 +0100

    Atomic reddit post transactions

--- backend/source/timeline/management/commands/retrieve_reddit_posts.py
+++ backend/source/timeline/management/commands/retrieve_reddit_posts.py
@@ -4,6 +4,7 @@ from typing import List, Tuple
 
 import praw
 import pytz
+from django.db import transaction
 
 from backup.models import RedditSource
 from timeline.management.commands.retrieve_posts import BasePostRetrievalCommand
@@ -27,26 +28,27 @@ class Command(BasePostRetrievalCommand):
         submissions = reddit.redditor(source.reddit_username).submissions.new(limit=None)
         updated_entries = []
         created_entries = []
-        for submission in submissions:
-            entry, created = Entry.objects.update_or_create(
-                schema=self.entry_schema,
-                extra_attributes__post_id=submission.id,
-                defaults={
-                    'title': submission.title,
-                    'description': submission.selftext,
-                    'date_on_timeline': datetime.fromtimestamp(submission.created_utc, pytz.UTC),
-                    'extra_attributes': {
-                        'post_id': submission.id,
-                        'post_score': submission.score,
-                        'post_community': submission.subreddit.display_name,
-                        'post_user': source.reddit_username,
-                        'post_url': submission.url,
+        with transaction.atomic():
+            for submission in submissions:
+                entry, created = Entry.objects.update_or_create(
+                    schema=self.entry_schema,
+                    extra_attributes__post_id=submission.id,
+                    defaults={
+                        'title': submission.title,
+                        'description': submission.selftext,
+                        'date_on_timeline': datetime.fromtimestamp(submission.created_utc, pytz.UTC),
+                        'extra_attributes': {
+                            'post_id': submission.id,
+                            'post_score': submission.score,
+                            'post_community': submission.subreddit.display_name,
+                            'post_user': source.reddit_username,
+                            'post_url': submission.url,
+                        }
                     }
-                }
-            )
-            if created:
-                created_entries.append(entry)
-            else:
-                updated_entries.append(entry)
+                )
+                if created:
+                    created_entries.append(entry)
+                else:
+                    updated_entries.append(entry)
 
         return created_entries, updated_entries
