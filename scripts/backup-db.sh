f88509c378421c0969c76db17dd9c440f46d53e1
Nicolas Bouliane <contact@nicolasbouliane.com>
2021-01-15 16:15:05 +0100

    Server side (temporary) permission fixes

--- scripts/backup-db.sh
+++ scripts/backup-db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 echo -e "\n\033[1mBacking up the database to ${1}\033[0m"
-docker-compose exec db pg_dump -c -U timeline-db > "$1"
+docker-compose exec timeline-db pg_dump -c -U postgres > "$1"
 echo "Done."
