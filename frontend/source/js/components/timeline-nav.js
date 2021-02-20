600f95f8c417797ba6ee47f943c551b86b0545c0
Nicolas Bouliane <contact@nicolasbouliane.com>
2021-02-20 11:35:17 +0100

    Mobile-friendly nav bar

--- frontend/source/js/components/timeline-nav.js
+++ frontend/source/js/components/timeline-nav.js
@@ -47,17 +47,17 @@ export default Vue.component('timeline-nav', {
     },
   },
   template: `
-    <div class="input-group">
-      <button class="button" @click="moveTimelineDate('years', -1)">-1Y</button>
-      <button class="button" @click="moveTimelineDate('months', -1)">-1M</button>
-      <button class="button" @click="moveTimelineDate('weeks', -1)">-1W</button>
-      <button class="button" @click="moveTimelineDate('days', -1)">-1D</button>
+    <div class="input-group timeline-nav">
+      <button class="minus-1-year year button" @click="moveTimelineDate('years', -1)">-1Y</button>
+      <button class="minus-1-month month button" @click="moveTimelineDate('months', -1)">-1M</button>
+      <button class="minus-1-week week button" @click="moveTimelineDate('weeks', -1)">-1W</button>
+      <button class="minus-1-day day button" @click="moveTimelineDate('days', -1)">-1D</button>
       <input class="input" type="date" v-model="timelineDateIso" :max="today.format('YYYY-MM-DD')">
-      <button class="button" :disabled="!showTomorrow" @click="pickTimelineDate(today)">Today</button>
-      <button class="button" :disabled="!showTomorrow" @click="moveTimelineDate('days', 1)">+1D</button>
-      <button class="button" :disabled="!showNextWeek" @click="moveTimelineDate('weeks', 1)">+1W</button>
-      <button class="button" :disabled="!showNextMonth" @click="moveTimelineDate('months', 1)">+1M</button>
-      <button class="button" :disabled="!showNextYear" @click="moveTimelineDate('years', 1)">+1Y</button>
+      <button class="today button" :disabled="!showTomorrow" @click="pickTimelineDate(today)">Today</button>
+      <button class="plus-1-day day button" :disabled="!showTomorrow" @click="moveTimelineDate('days', 1)">+1D</button>
+      <button class="plus-1-week week button" :disabled="!showNextWeek" @click="moveTimelineDate('weeks', 1)">+1W</button>
+      <button class="plus-1-month month button" :disabled="!showNextMonth" @click="moveTimelineDate('months', 1)">+1M</button>
+      <button class="plus-1-year year button" :disabled="!showNextYear" @click="moveTimelineDate('years', 1)">+1Y</button>
     </div>
   `
 });
