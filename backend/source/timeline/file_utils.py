4abcc0099aa41e6a74382bc2277e3c3b2eab8657
Nicolas Bouliane <contact@nicolasbouliane.com>
2021-03-17 16:34:04 +0100

    Put the right argument in the wrong place

--- backend/source/timeline/file_utils.py
+++ backend/source/timeline/file_utils.py
@@ -9,7 +9,6 @@ def generate_pdf_preview(input_path: Path, output_path: Path, max_dimensions: (i
     try:
         command = [
             'convert',
-            '-auto-orient',
             '-pointsize', '72',
             '-density', str(int(max_dimensions[0] / 8.5)),  # A4 pages are 8.5 inches wide
             '-units', 'PixelsPerInch',
@@ -107,6 +106,7 @@ def generate_image_preview(input_path: Path, output_path: Path, max_dimensions: 
     try:
         command = [
             'convert',
+            '-auto-orient',
             '-flatten',
             '-strip',
             '-thumbnail', f"{max_dimensions[0]}x{max_dimensions[1]}>",
