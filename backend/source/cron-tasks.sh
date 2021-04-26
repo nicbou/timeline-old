93b62a1a7f81b99af89acbbae2ad1f14c8009b34
Nicolas Bouliane <contact@nicolasbouliane.com>
2021-04-26 15:34:28 +0200

    Prevent cronjobs from running concurrently

--- backend/source/cron-tasks.sh
+++ backend/source/cron-tasks.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 source /etc/timeline-cronenv
-
-/usr/local/bin/python /usr/src/app/manage.py import
-/usr/local/bin/python /usr/src/app/manage.py export
+flock --verbose -n -w 21600 /etc/cronjobs.lock sh -c "/usr/local/bin/python /usr/src/app/manage.py import; /usr/local/bin/python /usr/src/app/manage.py export"
