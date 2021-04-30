c48c2ebb171351cee8dfa756b01d1124aab4acfb
Nicolas Bouliane <contact@nicolasbouliane.com>
2021-04-30 22:35:04 +0000

    Simpler, more reliable DB dump/restore

--- scripts/db-restore.sh
+++ scripts/db-restore.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 echo -e "\n\033[1mRestoring database from ${1}\033[0m"
-cat "$1" | docker-compose exec -T timeline-db psql -U postgres postgres
+cat "$1" | docker-compose exec -T timeline-db psql -U postgres
 echo "Done."
