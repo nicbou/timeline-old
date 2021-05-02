8719cd52957c7f750527a458bcc9a8c9f55dfb99
Nicolas Bouliane <contact@nicolasbouliane.com>
2021-05-02 14:45:55 +0200

    Fixed script path in copy-production-db

--- scripts/copy-production-db.sh
+++ scripts/copy-production-db.sh
@@ -6,4 +6,4 @@ scripts_dir=$(dirname "$0")
 today=$(date "+%Y-%m-%d")
 
 scp -P 2200 root@home.nicolasbouliane.com:/var/timeline/db-backups/backup-${today}.sql backup-${today}.sql
-bash ${scripts_dir}/restore-db.sh backup-${today}.sql
+bash ${scripts_dir}/db-restore.sh backup-${today}.sql
