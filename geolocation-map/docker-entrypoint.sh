694dbbb60388e33ffa390dae0892cbf0af0d33f4
Nicolas Bouliane <contact@nicolasbouliane.com>
2021-09-22 09:31:31 +0200

    Removed geolocation map

--- geolocation-map/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-crontab /etc/geolocation-crontab
-service cron start
-
-python -m http.server 80 -d "/var/www"
--- geolocation-map/source/refresh-map.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-location_items = requests.get(f"http://timeline-backend/timeline/entries.json/?schema=activity.location&limit=100&ordering=-date_on_timeline").json()['results']
-
-with open('/var/www/map/locations.json', 'w') as output_json_file:
-    json.dump(location_items, output_json_file)
