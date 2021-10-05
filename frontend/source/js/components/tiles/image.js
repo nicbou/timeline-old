78f498bd22c383e2e1d6fb4be24b37f927d587f6
Nicolas Bouliane <contact@nicolasbouliane.com>
2021-10-05 15:23:19 +0200

    Fixed image for entries without media.width

--- frontend/source/js/components/tiles/image.js
+++ frontend/source/js/components/tiles/image.js
@@ -10,15 +10,22 @@ export default Vue.component('image-tile', {
     imageSrcSet: function() {
       return `${this.entry.extra_attributes.previews.thumbnail} 1x, ${this.entry.extra_attributes.previews.thumbnail2x} 2x`;
     },
+    imageWidth: function() {
+      // Some entries can have a preview but no width. For example, a PDF has no width.
+      if(this.entry.extra_attributes.media && this.entry.extra_attributes.media.width && this.entry.extra_attributes.height){
+        return Math.floor(this.entry.extra_attributes.media.width/entry.extra_attributes.media.height * 200)
+      }
+    }
   },
   template: `
     <div class="image" v-if="entry.extra_attributes.previews">
       <img
         @click="$emit('select', entry)"
         :alt="entry.title"
+        :title="entry.title"
         :src="imageSrc"
         :srcset="imageSrcSet"
-        :width="Math.floor(entry.extra_attributes.media.width/entry.extra_attributes.media.height * 200)"
+        :width="imageWidth"
         height="200"
         />
       <div class="media-icons">
