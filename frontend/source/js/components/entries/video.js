ce2c95bb26f28382c5eb6e6769d1cbd52ca87dc6
Nicolas Bouliane <contact@nicolasbouliane.com>
2022-01-02 00:14:12 +0000

    Improved video entry style

--- frontend/source/js/components/entries/video.js
+++ frontend/source/js/components/entries/video.js
@@ -8,10 +8,14 @@ export default Vue.component('video-entry', {
   },
   template: `
     <div class="video" @click="$emit('select', entry)" v-if="entry.extra_attributes.previews">
-      <video-thumbnail :entry="entry" height="200"/>
-      <div class="media-icons">
-        <i v-if="hasGeolocation" class="fas fa-map-marker-alt"></i>
-        <i class="fas fa-play-circle"></i>
+      <i class="icon fas fa-play-circle"></i>
+      <div class="meta">{{ entry.title }}</div>
+      <div class="content">
+        <video-thumbnail :entry="entry" height="200"/>
+        <div class="media-icons">
+          <i v-if="hasGeolocation" class="fas fa-map-marker-alt"></i>
+          <i class="fas fa-play-circle"></i>
+        </div>
       </div>
     </div>
   `
