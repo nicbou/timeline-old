e7c26be4205ad0b97f9a68f2e8ceedd344eb48bf
Nicolas Bouliane <contact@nicolasbouliane.com>
2022-06-01 16:26:40 +0000

    Image fallback for devices that do not support embedded pdfs

--- frontend/source/js/components/previews/pdf.js
+++ frontend/source/js/components/previews/pdf.js
@@ -1,8 +1,12 @@
+import ImagePreview from './image.js';
+
 export default Vue.component('pdf-preview', {
   props: ['entry'],
   computed: {
   },
   template: `
-    <object :data="entry.extra_attributes.file.path" type="application/pdf"></object>
+    <object :data="entry.extra_attributes.file.path" type="application/pdf">
+      <image-preview :entry="entry"></image-preview>
+    </object>
   `
 });
